719d6ed8bbe8b0811dde12f67f1603c751c9ed3f
vitor <vitorsbsilva@gmail.com>
2021-09-02 16:59:54 -0300

    commit necessário

---
+++ 01_FUNDAMENTOS /BL.5 - JavaScript_ DOM, Eventos e Web Storage/scripts.js
@@ -0,0 +1,12 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "blue";
+
+// Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
+
+document.getElementById("page-title").innerText = "O Senhor dos Anéis";
+
+// Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo. 
+document.getElementById("second-paragraph").innerText = "É a melhor triologia já feita na história da humanidade";
+
+// Por fim, recupere o subtítulo e altere-o também.
+document.getElementById("subtitle").innerText = "By Tolkien";
